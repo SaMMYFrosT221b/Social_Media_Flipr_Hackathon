894ee3f318076168dfaf4bc45f3a47bc12559287
SaMMYFrosT221b <ratnakarg@iitbhilai.ac.in>
2024-03-03 00:36:07 +0530

    Made user Routes

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath, urlToHttpOptions } from "url";
 import authRoutes from "./routes/auth.js";
+
+import userRoutes from "./routes/users.js";
 import { register } from "./controllers/auth.js";
 
 // Configurations:
@@ -47,6 +49,8 @@ app.post("/auth/register", upload.single("picture"), register);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 // Mongos setup
 const PORT = process.env.PORT || 6000;
